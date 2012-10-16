8c60a61ca3e5a50cc64484ed9c0264cf851a139e
QLyine <noghost.sac@gmail.com>
2012-10-16 17:12:59 +0100

    SCG : OpenMpi -> Receiveing Arbitrary Size of Messages

--- scg/tp1/main.cpp
+++ scg/tp1/main.cpp
@@ -1,42 +1,73 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.cpp
- *
- *    Description:  TP1 - 2
- *
- *        Version:  1.0
- *        Created:  09/23/2012 08:41:00 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *        Company:  
- *
- * =====================================================================================
- */
-
+#include <mpi.h>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <fstream>
 #include <map>
-#include <utility>
+#include <vector>
+#include <stdlib.h>
+#include <math.h>
 
-using namespace std; 
+#define TAG 0
+#define WORKTAG 1
+#define DIETAG 2
+#define BUFSIZE 4
 
-int main(int argc, char* argv[]) {
+using namespace std;
+
+/* Local functions */
+
+static void master(void);
+static void slave(void);
+static void process_results(string result);
+static string do_work(string work);
+static char *getMsg(int id);
+static void sendMsg(int id, char *msg);
+
+int numprocs;
+int myrank;
+
+char ** filesList;
+int numberDocs;
+
+char *getMsg(int id)
+{
+
+  int msg_size;
+
+  char *buff;
+
+  MPI_Status stat;
+  MPI_Probe(id, TAG, MPI_COMM_WORLD, &stat);
+  MPI_Get_count(&stat, MPI_CHAR, &msg_size);
   
-  // declare variables
+  buff = (char *) calloc(msg_size, sizeof(char)); 
 
-  ifstream fi;
-  ofstream fo;
+  MPI_Recv(buff, msg_size, MPI_CHAR, id, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-  char delims [] = "\",;:|/\\.!()?_ ";
-  char *res;
+  return buff;
+}
 
-  string line;
+void sendMsg(int id, char *msg)
+{
+
+  MPI_Send(msg, strlen(msg), MPI_CHAR, id, TAG, MPI_COMM_WORLD);
+}
+
+int main(int argc, char* argv[])
+{
+
+
+
+  /* Initialize MPI */
+
+  MPI_Init(&argc, &argv);
+
+  /* Find out my identity in the default communicator */
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 
   if( argc < 2)
   {
@@ -46,52 +77,152 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
 
-  // declare maps
+  numberDocs = argc - 1;
+  filesList = &argv[1];
 
-  map <string, map <string, int> > m;
-  map <string, map <string, int> >::iterator mi;
-  map <string, int>::iterator mii;
-
-  // read all files and word count each
-
-  for( int i = 1; i < argc; i ++)
-  {
-    m[argv[i]] = map<string, int>();
+  if (myrank == 0) {
+    master();
+  } else {
+    slave();
   }
 
-  for( mi = m.begin(); mi != m.end(); ++mi )
-  {
-  //  cout << "Key: '" << iter->first << "', Value: " << iter->second << endl; 
-    fi.open(mi->first.c_str());
+  /* Shut down MPI */
 
-    while( fi.is_open() && !fi.eof() )
-    {
-      getline(fi, line);
-      res = strtok((char *) line.c_str(), delims);
-      while( res != NULL )
-      {
-        if( strlen(res) >= 3 ) mi->second[string(res)] ++;
-        res = strtok(NULL, delims);
-      }
-    }
-
-    fi.close();
-  }
-
-  // output result
-
-  ofstream fout("result.txt", ios::app); 
-
-  for( mi = m.begin(); mi != m.end(); ++mi )
-  {
-    cout << mi->first << " : " << endl;
-    fout << mi->first << " : " << endl;
-    for( mii = mi->second.begin(); mii != mi->second.end(); ++mii )
-    {
-      cout << "\t " << mii->first << " : " << mii->second << endl;
-      fout << "\t " << mii->first << " : " << mii->second << endl;
-    }
-  }
-
+  MPI_Finalize();
   return 0;
-} 
+}
+
+static void
+master(void)
+{
+  char end[] = "-";
+  vector<string> res;
+  char *buff;
+  int i, docsDone = 0, docsSent = 0; 
+ 
+  MPI_Status stat;
+
+  printf("->: We have %d processors and %d slaves \n", numprocs, (numprocs - 1));
+
+  printf("->: Waiting for Slaves .... \n");
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  printf("->: Sending jobs to each slave \n");
+
+  while( (numberDocs - docsDone) > 0 )
+  {
+    for(i=1;i<min(numprocs,(numberDocs - docsSent)+1);i++)
+    {
+      //METER A ENVIAR OUTROS FICHEIROS
+      sendMsg(i, filesList[docsSent]);
+      docsSent ++;
+
+    }
+    printf("->: Jobs sent %d \n", docsSent);
+    printf("->: Number of docs to send  %d \n", (numberDocs - docsSent) );
+    for(i=1;i<min(numprocs,(docsSent - docsDone)+1);i++)
+    {
+
+      buff = getMsg(i);
+
+      printf("->: %s\n", buff);
+      docsDone ++;
+      res.push_back( string(buff) );
+      
+      free(buff);
+    }
+  }
+
+
+  for(i = 1; i < numprocs; i++)
+  {
+    sendMsg(i, end);
+  }
+
+  printf("->: Commanded all slaves to end\n");  
+
+}
+
+static void
+slave(void)
+{
+  char *buff;
+  int numprocs;
+  int cmp = 1;
+
+  MPI_Status stat;
+  MPI_Barrier(MPI_COMM_WORLD);
+  /* receive from rank 0: */
+
+  buff = getMsg(0);
+  cmp = strcmp(buff, "-");
+
+  while( cmp != 0 )
+  {
+    string res = do_work(buff);
+
+    res += "\n";
+
+    sendMsg(0, (char *) res.c_str());
+
+    buff = getMsg(0);
+    cmp = strcmp(buff, "-");
+
+    free(buff);
+  }
+
+}
+
+
+static void 
+process_results(string result)
+{
+  //METER AQUI COISAS
+}
+
+
+static string
+do_work(string work)
+{
+  	map<string,int> m;
+	ifstream myReadFile;
+	char delims [] = "\",;:|/\\.!()?_ ";
+
+	myReadFile.open("sample.txt"); //ler do argumento
+	char output[100];
+
+	if(myReadFile.is_open())
+	{
+		string last; //trafulhice
+
+		while(!myReadFile.eof())
+		{
+			myReadFile >> output;
+			char* res = strtok(output, delims);
+			last = string(res); //trafulhice
+			if(res != NULL && strlen(res) >= 3)
+			{
+				
+				string str(res);
+				m[str]++;
+			}
+		}
+
+		if(strlen(last.c_str()) >= 3) //trafulhice
+			m[last]--;
+
+	}
+
+	myReadFile.close();
+	string res = work + ";";
+	map<string,int>::iterator it;
+	for ( it=m.begin() ; it != m.end(); it++ )
+	{
+		char buf[5];
+		
+		snprintf(buf, sizeof(buf), "%d", (*it).second);
+		res = res + (*it).first + ":" + buf + ";";
+	}
+
+	return res;
+}
